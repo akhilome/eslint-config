b32d84156362dc9dd18fc79a90d6e7ec42f768fa
Kizito Akhilome <kizito@akhilo.me>
2022-01-27 00:18:27 +0100

    init config

---
+++ index.js
@@ -0,0 +1,30 @@
+/*
+ * @rushstack/eslint-patch is used to include plugins as dev
+ * dependencies instead of imposing them as peer dependencies
+ *
+ * https://www.npmjs.com/package/@rushstack/eslint-patch
+ */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'jest'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'no-console': 'warn',
+  },
+  env: {
+    node: true,
+    jest: true,
+  },
+};
