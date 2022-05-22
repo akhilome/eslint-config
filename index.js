7a1508f840c4da7496e830d05497714239c4762f
Kizito Akhilome <kizito@akhilo.me>
2022-05-22 11:09:53 +0100

    rewrite config lib

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { rules, env } = require('./lib');
 /*
  * @rushstack/eslint-patch is used to include plugins as dev
  * dependencies instead of imposing them as peer dependencies
@@ -8,23 +9,8 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.eslint.json',
-  },
-  plugins: ['@typescript-eslint', 'prettier', 'jest'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:jest/recommended',
-    'plugin:prettier/recommended',
-  ],
-  rules: {
-    'no-console': 'warn',
-  },
-  env: {
-    node: true,
-    jest: true,
-  },
+  plugins: ['prettier', 'jest'],
+  extends: ['eslint:recommended', 'plugin:jest/recommended', 'plugin:prettier/recommended'],
+  rules,
+  env,
 };
---
+++ ts.js
@@ -0,0 +1,26 @@
+const { rules, env } = require('./lib');
+/*
+ * @rushstack/eslint-patch is used to include plugins as dev
+ * dependencies instead of imposing them as peer dependencies
+ *
+ * https://www.npmjs.com/package/@rushstack/eslint-patch
+ */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'jest'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules,
+  env,
+};
